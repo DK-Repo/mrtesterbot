00f7fc6754b08f195ef35d2a71ff412fa7967446
DK <dkom@wisebits.com>
2022-10-18 11:03:59 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "5507342368:AAEwWndBFT-S7H2Rt37NjNzVmyuZ_eTtzt4";
+const webAppUrl = "https://93.109.131.209:3000/";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (message) => {
+  const chatId = message.chat.id;
+  const text = message.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+      reply_markup: {
+        keyboard: [[{ text: "Заполни форму", web_app: { url: webAppUrl } }]],
+      },
+    });
+    await bot.sendMessage(
+      chatId,
+      "Заходи в наш интернет магазин по кнопке ниже",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Сделай заказ", web_app: { url: webAppUrl } }],
+          ],
+        },
+      },
+    );
+  }
+  bot.sendMessage(chatId, "Received yout message!");
+});
