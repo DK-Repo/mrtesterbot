a1d20ae3d3e76b67d9262ade75c02daabb2ada30
DK <dkom@wisebits.com>
2022-10-18 11:47:12 +0300

    temp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 import useTelegram from "./hooks/useTelegram";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
 
   return (
     <div className='App'>
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
